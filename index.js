0425926318c6d5a23020ffaa839a01edd1c74786
RISHITH125 <rishithp125@gmail.com>
2025-09-30 14:01:58 +0530

    changed cors allowed origins

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const app = express();
 const port = 8000;
 import planets from './routes/planets.js';
 
-app.use(cors({ 
-    origin: [
-        process.env.ALLOWED_ORIGIN1,
-        process.env.ALLOWED_ORIGIN2,
-    ].filter(Boolean)
-}))
+app.use(cors({
+  origin: (origin, callback) => {
+    console.log("Request origin:", origin);
+    if ([
+      process.env.ALLOWED_ORIGIN1,
+      process.env.ALLOWED_ORIGIN2
+    ].includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 app.use('/planets/', planets);
 
 app.listen(port, () => {
